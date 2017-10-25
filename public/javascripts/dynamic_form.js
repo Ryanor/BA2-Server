d0e249fb66bb05dffae5794759078adef4402856
Unknown <gerhard@abomate.com>
2017-10-25 19:41:38 +0200

    Extendend GUI with Characteristcs form
    Characteristics are added dynamicly but not correctly assigned to the adding service. Rethink implementation, maybe use pure Javascript code.

--- public/javascripts/dynamic_form.js
+++ public/javascripts/dynamic_form.js
@@ -1,18 +1,53 @@
-var counter = 0;
+var service = 1;
+var characteristic = 0;
+var descriptor = 0;
 
 function addService() {
-    counter++;
+    service++;
     var newFields = document.getElementById('serviceroot').cloneNode(true);
     newFields.id = '';
     newFields.style.display = 'block';
     var newField = newFields.childNodes;
-    for (var i=0;i<newField.length;i++) {
+    for (var i = 0; i < newField.length; i++) {
         var theName = newField[i].name;
         if (theName)
-            newField[i].name = theName + counter;
+            newField[i].name = theName + service;
     }
     var insertHere = document.getElementById('serviceend');
-    insertHere.parentNode.insertBefore(newFields,insertHere);
+    insertHere.parentNode.insertBefore(newFields, insertHere);
 }
 
-window.onload = addService;
+function addCharacteristic() {
+    characteristic++;
+    var newFields = document.getElementById('characteristicroot').cloneNode(true);
+    newFields.id = '';
+    newFields.style.display = 'inline';
+    var newField = newFields.childNodes;
+    for (var i = 0; i < newField.length; i++) {
+        var theName = newField[i].name;
+        if (theName)
+            newField[i].name = theName + characteristic;
+    }
+    var insertHere = document.getElementById('characteristicend');
+    insertHere.parentNode.insertBefore(newFields, insertHere);
+}
+
+function addDescriptor() {
+
+}
+
+function removeService(context) {
+    if (service != 1) {
+        service--;
+        context.parentNode.parentNode.removeChild(context.parentNode);
+    }
+}
+
+function removeCharacteristic(context) {
+    characteristic--;
+    context.parentNode.parentNode.removeChild(context.parentNode);
+}
+
+
+
+
