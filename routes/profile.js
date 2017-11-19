ebb075194d3ec94f878bb9669af106956195c6d4
Unknown <gerhard@abomate.com>
2017-11-19 17:28:23 +0100

    Added values to one test profile

--- routes/profile.js
+++ routes/profile.js
@@ -12,6 +12,12 @@ router.get('/json', function (req, res, next) {
     res.sendFile(path.join(__dirname, '../public/profiles', 'myjsonfile.json'));
 });
 
+router.get('/json1', function (req, res, next) {
+    res.set('Content-Type', 'application/json');
+    //var parser = new xml2js.Parser();
+    res.sendFile(path.join(__dirname, '../public/profiles', 'test_profile.json'));
+});
+
 router.get('/xml', function (req, res, next) {
     res.set('Content-Type', 'application/xml');
     res.sendFile(path.join(__dirname, '../public/profiles', 'test_profile.xml'));
