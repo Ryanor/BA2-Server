aa4e5f9d96cd8790056dac2fe29c5f01d6f8a3b4
Unknown <gwuehrer@gmx.at>
2017-11-22 21:28:32 +0100

    Added mongobd to project
    Established connection to mongodb

--- public/javascripts/dynamic_form.js
+++ public/javascripts/dynamic_form.js
@@ -6,13 +6,13 @@ function addService() {
     newFields.id = '';
     newFields.style.display = 'block';
     var newField = newFields.childNodes;
-    for (var i=0;i<newField.length;i++) {
+    for (var i = 0; i < newField.length; i++) {
         var theName = newField[i].name;
         if (theName)
             newField[i].name = theName + counter;
     }
     var insertHere = document.getElementById('serviceend');
-    insertHere.parentNode.insertBefore(newFields,insertHere);
+    insertHere.parentNode.insertBefore(newFields, insertHere);
 }
 
 window.onload = addService;
