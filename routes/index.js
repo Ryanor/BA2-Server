c366df518f6d5f886f805cdc94561ff2d09500cf
Unknown <gwuehrer@gmx.at>
2018-02-03 22:37:06 +0100

    Another try to get exit code or messages from script

--- routes/index.js
+++ routes/index.js
@@ -33,14 +33,9 @@ router.get('/profiles', function (req, res) {
  */
 router.get('/checkSimulator', function (req, res) {
     console.log("Checking for running simulator...");
-    process.execFile(path.join(__dirname, '../','isSimulatorRunning.sh'), ['/usr/bin/node', '/home/pi/project/BA2-Simulator/main.js'] , function(err, stdout, stderr) {
-        // Node.js will invoke this callback when the
-        if(err) {
-            console.log(err);
-            throw err;
-        }
-        console.log(stdout);
-        console.log(stderr);
+    var running = process.execFile(path.join(__dirname, '../','isSimulatorRunning.sh'), ['/usr/bin/node', '/home/pi/project/BA2-Simulator/main.js']);
+    running.on('close', function (code) {
+        console.log(code);
     });
     res.status(200).send("dfada");
 });
